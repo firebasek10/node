6f11bccc05efb1925b2b8084a562684d23caa7b7
firebasek10 <firebasek10@gmail.com>
2021-10-20 04:38:14 +0530

    updated

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require("express");
 const app = express(); 
-
-
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const cors = require("cors");
 mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connection Success 👍'))
 .catch((err) =>{
     console.log(err);
 });
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
