96098883dc6ab78ec68c5126cd2456a9b0d08f13
firebasek10 <firebasek10@gmail.com>
2021-10-19 09:21:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require("express");
+const app = express(); 
+
+
+mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connection Success 👍'))
+.catch((err) =>{
+    console.log(err);
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
