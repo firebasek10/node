d22f0ac3add50f53c36230d26534cc3c4f323b6c
firebasek10 <firebasek10@gmail.com>
2021-10-19 09:23:50 +0530

    Fixed pages

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express(); 
 
 
-mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connection Success 👍'))
+mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connection Success '))
 .catch((err) =>{
     console.log(err);
 });
