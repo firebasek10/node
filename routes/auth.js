99d4b3a0e584b8d1eb0a80493aa563479ca37dd9
firebasek10 <firebasek10@gmail.com>
2021-10-20 05:43:17 +0530

    updatejd

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -43,8 +44,14 @@ router.post('/login', async (req, res) => {
 
         
       originalPassword !== req.body.password && res.status(401).json("Wrong Password");
+
+      const accessToken = jwt.sign({
+          id:user._id,isAdmin:user.isAdmin,
+      },
+      process.env.JWT_SEC,{expiresIn:"30d"}
+      );
       const { password, ...others } = user._doc; 
-            res.status(200).json(others);
+            res.status(200).json({...others, accessToken});
             
     }catch(err){
         res.status(500).json(err);
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express(); 
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+
 const cors = require("cors");
 mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connection Success 👍'))
 .catch((err) =>{
@@ -15,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
